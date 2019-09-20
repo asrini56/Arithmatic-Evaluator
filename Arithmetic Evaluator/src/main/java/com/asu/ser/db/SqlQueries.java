43a8646fc5c8259a8ac50798739f023bdfca7c50
Akhilesh Krishnan <akhilesh@akhileshs-mbp.3521.dhcp.asu.edu>
2019-09-20 15:06:18 -0700

    1. Adding a java file to save and fetch SQL queires. 2. Adding a java file to save and execute table creation queries.

---
+++ Arithmetic Evaluator/src/main/java/com/asu/ser/db/SqlQueries.java
@@ -0,0 +1,11 @@
+package com.asu.ser.db;
+
+public class SqlQueries {
+	
+	
+	// use %s for unknown values and use String.format(Query, params) to replace %s in query. 
+	
+	//TABLE USERS
+	//public static final String SELECT_USER = "select * from Users where ID = %s";
+
+}
