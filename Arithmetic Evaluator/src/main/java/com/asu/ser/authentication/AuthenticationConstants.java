e01916a9c01687de38276dcfc77ab0b7fbf1c627
akrish84 <akrish84@asu.edu>
2019-09-28 21:47:47 -0700

    Moving authentication related constants to a separate file as they are used accross multiple classes

---
+++ Arithmetic Evaluator/src/main/java/com/asu/ser/authentication/AuthenticationConstants.java
@@ -0,0 +1,20 @@
+package com.asu.ser.authentication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthenticationConstants {
+
+	public static final String ATTR_NAME_USER_TOKEN = "USER_TOKEN";
+	public static final String ATTR_NAME_USERNAME = "USERNAME";
+	public static final int AUTHENTICATION_FAILED_CODE = 601;
+	public static final List<String> EXCLUDED_URLS;
+	
+	static {
+		EXCLUDED_URLS = new ArrayList<>();
+		EXCLUDED_URLS.add("jsp");
+		EXCLUDED_URLS.add("css");
+		EXCLUDED_URLS.add("js");
+		EXCLUDED_URLS.add("html");
+	}
+}
